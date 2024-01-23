60b82777cf96bc9387f49ad9c63d55ddad9db694
Hasse <imberg.hasse@pm.me>
2024-01-23 19:19:11 +0200

    first commit

---
+++ personal-websites-v2/src/pages/index.tsx
@@ -0,0 +1,5 @@
+/* Quick Introduction: A brief but powerful intro to who you are and what you do.
+Skill Highlights: Showcase your top skills or technologies you excel in.
+Portfolio Teasers: Preview your best projects with links to detailed pages.
+About Me Snippet: A short personal bio leading to the full 'About Me' page.
+Visually Engaging: Use animations, high-quality images, or interactive elements to captivate visitors. */
