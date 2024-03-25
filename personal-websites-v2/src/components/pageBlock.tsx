9c2db7c2b9eaed60f17a3ee46ceb2f744e871209
Hasse <imberg.hasse@pm.me>
2024-03-25 11:01:37 +0200

    etusivu kuva parannukset

--- personal-websites-v2/src/components/pageBlock.tsx
+++ personal-websites-v2/src/components/pageBlock.tsx
@@ -1,19 +1,32 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function PageBlock({
   title,
   textContent,
+  imageSrc,
   btn = null,
 }: {
   title: string;
   textContent: string;
+  imageSrc?: string;
   btn?: { href: string; text: string } | null;
 }) {
   return (
     <div className="p-4">
       {" "}
       {/* Image placeholder */}
-      <div className="bg-gray-800 shadow-xl h-48 w-full mb-4 mt-5"></div>{" "}
+      {imageSrc && (
+        <div className="flex justify-center rounded-md border  bg-slate-900 items-center">
+          <Image
+            className=" shadow-md"
+            src={imageSrc}
+            width={800}
+            height={800}
+            alt="Image"
+          />
+        </div>
+      )}
       <h3 className="text-2xl font-bold mb-2">{title}</h3>
       <p className="mb-4 text-justify">{textContent}</p>
       {btn && (
--- personal-websites-v2/src/pages/index.tsx
+++ personal-websites-v2/src/pages/index.tsx
@@ -12,15 +12,6 @@ import {
   socialsText,
 } from "@/textContents/indexTextContents";
 
-// Titles:
-const IntroductionTitle = `Welcome - about me`;
-const expAndProjTitle = `Practical Experience & Projects Portfolio`;
-const collabTitle = `Communication & Collaboration`;
-const devOpsTitle = `DevOps`;
-const techStackTitle = `Tech Stack`;
-const certificatesTitle = `Certificates`;
-const socialsTitle = `Social Accounts`;
-
 // Buttons
 const techStackButton = { href: "/techStack", text: "Full tech stack" };
 const expAndProjButton = { href: "/portfolio", text: "Full portfolio" };
@@ -28,26 +19,33 @@ const expAndProjButton = { href: "/portfolio", text: "Full portfolio" };
 const Index: React.FC = () => {
   return (
     <div className="max-w-6xl mx-auto py-4 px-2 sm:px-4">
-      <PageBlock title={IntroductionTitle} textContent={IntroductionText} />
+      <PageBlock
+        title={`Welcome - about me`}
+        textContent={IntroductionText}
+        imageSrc={"/selfie.jpg"}
+      />
       <div className="grid md:grid-cols-2">
         <PageBlock
-          title={expAndProjTitle}
+          title={`Practical Experience & Projects Portfolio`}
           textContent={expAndProjectsText}
           btn={expAndProjButton}
         />
         <PageBlock
-          title={techStackTitle}
+          title={`Tech Stack`}
           textContent={techStackText}
           btn={techStackButton}
         />
       </div>
       <div className="grid md:grid-cols-2">
-        <PageBlock title={collabTitle} textContent={collabText} />
-        <PageBlock title={devOpsTitle} textContent={devOpsText} />
+        <PageBlock
+          title={`Communication & Collaboration`}
+          textContent={collabText}
+        />
+        <PageBlock title={`DevOps`} textContent={devOpsText} />
       </div>
       <div className="grid md:grid-cols-2">
-        <PageBlock title={certificatesTitle} textContent={certificatesText} />
-        <PageBlock title={socialsTitle} textContent={socialsText} />
+        <PageBlock title={"Certificates"} textContent={certificatesText} />
+        <PageBlock title={`Social Accounts`} textContent={socialsText} />
       </div>
     </div>
   );
