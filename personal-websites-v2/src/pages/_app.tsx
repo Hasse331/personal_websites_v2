0b30608fa4f64a086906422535a56deb59fd91c9
Hasse <imberg.hasse@pm.me>
2024-03-25 15:43:42 +0200

    changes to filestructure

--- personal-websites-v2/src/pages/_app.tsx
+++ personal-websites-v2/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
-import "../app/globals.css"; // Adjust the import path if necessary
-import RootLayout from "@/app/layout"; // Ensure the path is correct
+import "../globals.css"; // Adjust the import path if necessary
+import RootLayout from "@/layout"; // Ensure the path is correct
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
